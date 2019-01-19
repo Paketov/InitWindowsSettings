504622f3c3f93306d75c69108161c8d2b50d9fa4
s <Shlixterberg@gmail.com>
2019-01-19 04:20:34 +0400

    update

--- iSCSI/connect_iscsi.bat
+++ iSCSI/connect_iscsi.bat
@@ -1,3 +1,6 @@
+netsh advfirewall firewall delete rule name="Allow_iSCSI"
+netsh advfirewall firewall add rule name="Allow_iSCSI" dir=out action=allow protocol=TCP remoteport=3260
+
 sc config "MSiSCSI" start=auto
 sc start "MSiSCSI"
 timeout 7
--- iSCSI/disconnect_iscsi.bat
+++ iSCSI/disconnect_iscsi.bat
@@ -6,3 +6,5 @@ powershell -Command "Get-IscsiTarget | Disconnect-IscsiTarget -Confirm:$false"
 powershell -Command "Get-IscsiTargetPortal | Remove-IscsiTargetPortal -Confirm:$false"
 sc config "MSiSCSI" start=demand
 sc stop "MSiSCSI"
+
+netsh advfirewall firewall delete rule name="Allow_iSCSI"
