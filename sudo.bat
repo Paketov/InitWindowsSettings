89ca625e8fb466cc57b9bda590e3e0fc754bf733
s <Shlixterberg@gmail.com>
2019-03-10 15:18:21 +0400

    updt

--- sudo.bat
+++ sudo.bat
@@ -3,12 +3,22 @@
 set t=%1
 set t2="%t:"=%"
 
-for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_FIRST=%%b
+set t4=
+set t5=
+:loop
+ shift
+ if "t5" == "" (
+  set t4=%1 
+ ) else (
+  set t4=%t4% %1
+ )
+ set t5=1
+if not "%~1"=="" goto loop
 
-IF "%ALL_BUT_FIRST%"=="" (
-	set t3=
+IF "%t5%"=="" (
+ set t3=
 ) ELSE (
-	set t3=%ALL_BUT_FIRST:"=" ^&chr(34)^& "%
+ set t3=%t4:"=" ^&chr(34)^& "%
 )
 
 echo Set objShell = CreateObject("Shell.Application") > %temp%\sudo.vbs
